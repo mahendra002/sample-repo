489b6aa5008ef07bb987509804f31d9ef180eb9a
mahendra002 <singh.m0212@gmail.com>
2021-06-10 20:59:42 +0530

    setup sample-repo

---
+++ JS and React/src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript And React.js</h1>
+      <p>
+        This bootcamp was taken by Shape AI from 25 May. It was an amazing
+        Bootcamp. We learnt Html, JavaScript and React JS.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
